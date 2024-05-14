ce3df033048b32447f50ecc0da2e0721e6e0bc49
Your Name <you@example.com>
2024-05-14 19:03:06 +0530

    edited index page and added contents page

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path("",views.index,name="index")
+    path("wiki",views.index,name="index"),
+    path("wiki/<str:name>",views.page,name="page"),
     
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from . import util
+from django.http import Http404
+
 
 # Create your views here.
 def index(request):
-    return render(request,"encyclopedia/index.html")
+    return render(request,"encyclopedia/index.html",{"entries":util.list_entries()})
+
+def page(request,name):
+    if util.get_entry(name) != None:
+        return render(request,"encyclopedia/page.html",{
+            "name":name,
+            "content":util.to_HTML(name)
+        })
+    else:
+        raise Http404(f"this page does not exist.")
